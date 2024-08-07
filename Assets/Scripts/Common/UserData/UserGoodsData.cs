eaf6198371a1852d0554ed109cb25bf4f78dc28f
박문수 <b1372312369@gmail.com>
2024-08-07 11:05:02 +0900

    [Update] UserData추가

---
+++ Assets/Scripts/Common/UserData/UserGoodsData.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGoodsData : IUserData
+{
+    //골드등 유저 소지 데이터
+    public long Gold { get; set; }
+    
+    public void SetDefaultData()
+    {
+        Logger.Log($"{GetType()::SetDefaultData}");
+        Gold = 0;
+    }
+
+    public bool LoadData()
+    {
+        Logger.Log($"{GetType()::LoadData}");
+
+        bool result = false;
+
+        try
+        {
+            Gold = long.Parse(PlayerPrefs.GetString("Gold"));
+            result = true;
+            
+            Logger.Log($"Gold:{Gold}");
+        }
+        catch (Exception e)
+        {
+            Logger.Log($"Load failed ({e.Message})");
+        }
+        return result;
+    }
+
+    public bool SaveData()
+    {
+        
+    }
+}
